e35923ca4cbeab4deee4504c07da653e499a1ae0
MelissaCFox <melissa.caitlin.fox@gmail.com>
2022-03-10 10:32:22 -0600

    completed all fixtures

--- aceapi/models/student_test.py
+++ aceapi/models/student_test.py
@@ -4,7 +4,7 @@ from aceapi.models import AppUser, Test
 class StudentTest(models.Model):
     student = models.ForeignKey(AppUser, on_delete=models.CASCADE)
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
-    english = []
-    math = []
-    reading = []
-    science = []
+    english = models.CharField(max_length=20)
+    math = models.CharField(max_length=20)
+    reading = models.CharField(max_length=20)
+    science = models.CharField(max_length=20)
