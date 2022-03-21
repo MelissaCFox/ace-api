5a211de3511c8bb13cf107598a6300134664beb9
MelissaCFox <melissa.caitlin.fox@gmail.com>
2022-03-21 18:35:48 -0500

    submitter field for student tests

--- aceapi/models/student_test.py
+++ aceapi/models/student_test.py
@@ -2,7 +2,7 @@ from django.db import models
 from aceapi.models import AppUser, Test
 
 class StudentTest(models.Model):
-    student = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    student = models.ForeignKey(AppUser, on_delete=models.CASCADE, related_name="tests")
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
     english = models.CharField(max_length=20)
     math = models.CharField(max_length=20)
